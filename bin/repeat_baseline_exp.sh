1bb6082d2d29aa169224afc56bad07b60251a7ae
alecokas <alecokastanos@gmail.com>
2021-01-19 23:28:18 +0000

    Fix up repeats for multiple seeds

--- bin/repeat_baseline_exp.sh
+++ bin/repeat_baseline_exp.sh
@@ -10,8 +10,9 @@ label_proportion=$5
 doc2vec_dm=$6
 
 
-for seed in 12321 19291 48911
+for seed in 12321 19291 48911 1403 1912
 do
     python src/train_baseline.py "$name"_seed_"$seed" --model "$model" --input-data-dir "$input_data_dir" \
-    --stemmer-path "$stemmer_path" --train-set-label-proportion $label_proportion --doc2vec-dm $doc2vec_dm 
+    --stemmer-path "$stemmer_path" --train-set-label-proportion $label_proportion --doc2vec-dm $doc2vec_dm \
+    --seed $seed
 done
---
+++ bin/repeat_gnn_exp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Run the one-hot GCN experiment a number of times with different seeds
+
+name=$1
+input_data_dir=$2
+stemmer_path=$3
+epochs=$4
+lr=$5
+dropout_ratio=$6
+label_proportion=$7
+
+for seed in 12321 19291 48911 1403 1912
+do 
+    python src/train_gnn.pt "$name"_repeat_gnn_exp --input-data-dir "$input_data_dir" --input-features one-hot \
+    --stemmer-path "$stemmer_path" --train-dir train_"$seed" --epochs $epochs --lr $lr --dropout-ratio $dropout_ratio \
+    --train-set-label-proportion $label_proportion --seed $seed
+done
