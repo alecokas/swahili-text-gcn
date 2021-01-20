98c4ede88ca64cf97848b3283758674de4ea8ac2
alecokas <alecokastanos@gmail.com>
2021-01-20 19:35:35 +0000

    Add option to train as text2vec

--- bin/repeat_gnn_exp.sh
+++ bin/repeat_gnn_exp.sh
@@ -9,10 +9,11 @@ epochs=$4
 lr=$5
 dropout_ratio=$6
 label_proportion=$7
+input_features=$8
 
 for seed in 12321 19291 48911 1403 1912
 do 
-    python src/train_gnn.py "$name"_repeat_gnn_exp --input-data-dir "$input_data_dir" --input-features one-hot \
+    python src/train_gnn.py "$name"_repeat_gnn_exp --input-data-dir "$input_data_dir" --input-features "$input_features" \
     --stemmer-path "$stemmer_path" --train-dir train_"$seed" --epochs $epochs --lr $lr --dropout-ratio $dropout_ratio \
     --train-set-label-proportion $label_proportion --seed $seed
 done
